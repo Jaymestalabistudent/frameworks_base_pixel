fb5410973c72dd7467988e352778898800c6a337
Jyotiraditya <jyotiraditya@aospa.co>
2020-10-30 11:30:59 -0300

    OrchidOS Sound : Introduce Adaptive Playback [1/2]
    Automatically pause media when muted. Resume if unmuted within a minute.
    
    Co-Authored-By: Stylogey <stylogey@gmail.com>
    Change-Id: I5d37478b3739309dcaae4eb6a0e4aac6c87f120f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5739,7 +5739,23 @@ public final class Settings {
          */
         @Readable
         public static final String ARCANE_IDLE_MANAGER = "arcane_idle_manager";
-        
+       
+        /**
+        * Adaptive playback
+        * Automatically pause media when the volume is muted and
+        * will resume automatically when volume is restored.
+        *   0 = disabled
+        *   1 = enabled
+        * @hide
+        */
+       public static final String ADAPTIVE_PLAYBACK_ENABLED = "adaptive_playback_enabled";
+
+       /**
+        * Adaptive playback's timeout in ms
+        * @hide
+        */
+       public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
+
         /**
 	 * Settings Base Layout
 	 * @hide
@@ -5932,6 +5948,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(HIGH_TOUCH_POLLING_RATE_ENABLE);
             PRIVATE_SETTINGS.add(SENSOR_BLOCK);
             PRIVATE_SETTINGS.add(LOCKSCREEN_PIN_SCRAMBLE_LAYOUT);
+            PRIVATE_SETTINGS.add(ADAPTIVE_PLAYBACK_ENABLED);
+            PRIVATE_SETTINGS.add(ADAPTIVE_PLAYBACK_TIMEOUT);
         }
 
         /**
