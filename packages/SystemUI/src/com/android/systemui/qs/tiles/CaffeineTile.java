ee631caf14a324bedf09f906a9181d946ddbc644
Keith Mok <kmok@cyngn.com>
2020-11-10 10:47:09 -0300

    OrhidOs QS : Add caffeine QS tile
    * Caffeine Tile icon designed by Andrew Fluck
    
    [Adin Kwok] Add long press to infinity to caffeine qs tile
    [ghostrider-reborn] Updated for android 12
    [Stallix] Adapt for A13 QPR3
    
    Change-Id: If54ea27ddf179c2de39b61850163e14173727698
    Signed-off-by: Adithya <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Co-Authored-By: Adin Kwok <adin@adin.dev>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -34,16 +34,18 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 import com.android.internal.logging.MetricsLogger;
-import com.android.systemui.R;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.qs.QsEventLogger;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
-import com.android.systemui.qs.QSHost;
-import com.android.systemui.qs.logging.QSLogger;
-import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
 
 import javax.inject.Inject;
 
@@ -63,7 +65,6 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
         30 * 60,  // 30 min
         -1,       // infinity
     };
-    private static final int INFINITE_DURATION_INDEX = DURATIONS.length - 1;
     private CountDownTimer mCountdownTimer = null;
     public long mLastClickTime = -1;
     private final Receiver mReceiver = new Receiver();
@@ -71,6 +72,7 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
     @Inject
     public CaffeineTile(
             QSHost host,
+            QsEventLogger uiEventLogger,
             @Background Looper backgroundLooper,
             @Main Handler mainHandler,
             FalsingManager falsingManager,
@@ -79,7 +81,7 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
             ActivityStarter activityStarter,
             QSLogger qsLogger
     ) {
-        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+        super(host, uiEventLogger, backgroundLooper, mainHandler, falsingManager, metricsLogger,
                 statusBarStateController, activityStarter, qsLogger);
         mWakeLock = mContext.getSystemService(PowerManager.class).newWakeLock(
                 PowerManager.FULL_WAKE_LOCK, "CaffeineTile");
@@ -102,11 +104,7 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    public void handleSetListening(boolean listening) {
-    }
-
-    @Override
-    protected void handleClick(@Nullable View view) {
+    public void handleClick(@Nullable View view) {
         // If last user clicks < 5 seconds
         // we cycle different duration
         // otherwise toggle on/off
@@ -145,15 +143,18 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleLongClick(@Nullable View view) {
-        if (mWakeLock.isHeld()) {
-            if (mDuration == INFINITE_DURATION_INDEX) {
-                return;
-            }
-        } else {
+        // Set duration to infinity on long click
+        int infinityIndex = DURATIONS.length - 1;
+        if (mLastClickTime == infinityIndex) {
+            // Already at infinity
+            return;
+        }
+        mDuration = infinityIndex;
+        startCountDown(DURATIONS[mDuration]);
+        if (!mWakeLock.isHeld()) {
             mWakeLock.acquire();
         }
-        mDuration = INFINITE_DURATION_INDEX;
-        startCountDown(DURATIONS[INFINITE_DURATION_INDEX]);
+        mLastClickTime = SystemClock.elapsedRealtime();
         refreshState();
     }
 
