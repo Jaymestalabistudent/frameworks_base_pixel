c378a24441770920ed988ec608afd1609532f395
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-12 14:30:29 +0000

    OrchidOs Feature: reticker [1/2]
    [someone5678]
    * Based on crDroid Android's cleaned A12 commit[1]
    * Forward port to Android 13 QPR3
    * Follow the Material You styles
    
    Original commit:
    https://github.com/Descendant-XI/frameworks_base/commit/6ca0cff0da94d07a14ca8695b7bde5eaea9deef5
    
    Ref:
    [1] https://github.com/crdroidandroid/android_frameworks_base/commit/e44ede735e177d8297cfbfa8b22a21041c229ace
    
    Change-Id: I582d0484977f0e850d158e8645171e638fcc0c4c
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>
    
    Properly update view for reTicker
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    reTicker: Do not show when content/icon is null
    
    Change-Id: I4ece6be5afe4e51f0506768563dc1277fdb0f4d1
    Co-Authored-By: Barba_tech <91166081+ageuqxb@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6046,6 +6046,18 @@ public final class Settings {
 	 * @hide
 	 */
 	public static final String DISABLE_USERCARD = "disable_usercard";
+        
+        /**
+         * reTicker Status
+         * @hide
+         */
+        public static final String RETICKER_STATUS = "reticker_status";
+
+        /**
+         * reTicker Colored
+         * @hide
+         */
+        public static final String RETICKER_COLORED = "reticker_colored";
 
        
         /**
