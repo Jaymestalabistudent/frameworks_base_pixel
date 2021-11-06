a51f4060d7f498f05b3ce5b15ae46b654d1a2d6a
SagarMakhar <sagarmakhar@gmail.com>
2021-11-06 14:09:41 +0000

    OrchidOs QS :  unlocking to use sensitive QS tiles optional [1/2]
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I355ef643fc046ce50f40924db130ad212fea5800
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    SystemUI: Improve Secure QS tile behaviour toggle [1/2]
    
    * Instead of checking settings in handleClick, added a settings observer and updates tiles on change
    * Moved the setting key to Settings.Secure class since it's more apt
    * Also guarded so debug logs
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-Authored-By: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -224,6 +224,7 @@ public class SecureSettings {
         Settings.Secure.BLUETOOTH_LE_BROADCAST_APP_SOURCE_NAME,
         Settings.Secure.LOCK_SCREEN_WEATHER_ENABLED,
         Settings.Secure.ACCESSIBILITY_FONT_SCALING_HAS_BEEN_CHANGED,
-        Settings.Secure.GESTURE_NAVBAR_LENGTH_MODE
+        Settings.Secure.GESTURE_NAVBAR_LENGTH_MODE,
+        Settings.Secure.DISABLE_SECURE_TILES_ON_LOCKSCREEN
     };
 }
