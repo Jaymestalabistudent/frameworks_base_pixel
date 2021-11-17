c36e3d1641a19322af58de486ccc3ddeb95f9da3
Ido Ben-Hur <idoybh2@gmail.com>
2021-11-17 14:42:28 +0200

    OrchidOs QS: QuickStatusBarHeader: Add date & clock click actions
    commit b5c851019d242e8b9e6e98910eb3fe12f0acbd89
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date:   Wed Nov 17 14:42:28 2021 +0200
    
        QuickStatusBarHeader: Add date & clock click actions
    
        Open default clock & calendar accordingly
        Long click date code partially taken from: https://github.com/yaap/frameworks_base/commit/d5f25c8af8e6026728f6a9e44c1facef5b6b1043
        Giving co-authorship
    
        Co-authored-by: calebcabob <calphonic@gmail.com>
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    commit 7d6d8f769cce9a979aaad4b738dea4140af3911e
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date:   Thu Dec 16 22:05:49 2021 +0200
    
        QuickStatusBarHeader: Adapt date & clock click actions to r18
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Co-authored-by: calebcabob <calphonic@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -18,8 +18,16 @@ import static android.app.StatusBarManager.DISABLE2_QUICK_SETTINGS;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.AlarmClock;
+import android.provider.CalendarContract;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -28,26 +36,106 @@ import android.widget.FrameLayout;
 
 import com.android.systemui.R;
 import com.android.systemui.util.LargeScreenUtils;
+import com.android.systemui.battery.BatteryMeterView;
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.qs.QSDetail.Callback;
+import com.android.systemui.statusbar.phone.StatusBarContentInsetsProvider;
+import com.android.systemui.statusbar.phone.StatusBarIconController.TintedIconManager;
+import com.android.systemui.statusbar.phone.StatusIconContainer;
+import com.android.systemui.statusbar.policy.Clock;
+import com.android.systemui.statusbar.policy.VariableDateView;
+
+import java.util.List;
 
 /**
  * View that contains the top-most bits of the QS panel (primarily the status bar with date, time,
  * battery, carrier info and privacy icons) and also contains the {@link QuickQSPanel}.
  */
-public class QuickStatusBarHeader extends FrameLayout {
+public class QuickStatusBarHeader extends FrameLayout implements
+        View.OnClickListener, View.OnLongClickListener {
 
     private boolean mExpanded;
     private boolean mQsDisabled;
 
     protected QuickQSPanel mHeaderQsPanel;
+    private View mDatePrivacyView;
+    private View mDateView;
+    // DateView next to clock. Visible on QQS
+    private VariableDateView mClockDateView;
+    private View mSecurityHeaderView;
+    private View mStatusIconsView;
+    private View mContainer;
+
+    private View mQSCarriers;
+    private ViewGroup mClockContainer;
+    private Clock mClockView;
+    private Space mDatePrivacySeparator;
+    private View mClockIconsSeparator;
+    private boolean mShowClockIconsSeparator;
+    private View mRightLayout;
+    private View mDateContainer;
+    private View mPrivacyContainer;
+
+    private BatteryMeterView mBatteryRemainingIcon;
+    private StatusIconContainer mIconContainer;
+    private View mPrivacyChip;
+
+    private TintedIconManager mTintedIconManager;
+    private QSExpansionPathInterpolator mQSExpansionPathInterpolator;
+    private StatusBarContentInsetsProvider mInsetsProvider;
+
+    private int mRoundedCornerPadding = 0;
+    private int mWaterfallTopInset;
+    private int mCutOutPaddingLeft;
+    private int mCutOutPaddingRight;
+    private float mKeyguardExpansionFraction;
+    private int mTextColorPrimary = Color.TRANSPARENT;
+    private int mTopViewMeasureHeight;
+
+    @NonNull
+    private List<String> mRssiIgnoredSlots;
+    private boolean mIsSingleCarrier;
+
+    private boolean mHasCenterCutout;
+    private boolean mConfigShowBatteryEstimate;
+
+    private boolean mUseCombinedQSHeader;
+    private final ActivityStarter mActivityStarter;
+    private final Vibrator mVibrator;
 
     public QuickStatusBarHeader(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mActivityStarter = Dependency.get(ActivityStarter.class);
+        mVibrator = context.getSystemService(Vibrator.class);
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
         mHeaderQsPanel = findViewById(R.id.quick_qs_panel);
+        mDatePrivacyView = findViewById(R.id.quick_status_bar_date_privacy);
+        mStatusIconsView = findViewById(R.id.quick_qs_status_icons);
+        mQSCarriers = findViewById(R.id.carrier_group);
+        mContainer = findViewById(R.id.qs_container);
+        mIconContainer = findViewById(R.id.statusIcons);
+        mPrivacyChip = findViewById(R.id.privacy_chip);
+        mDateView = findViewById(R.id.date);
+        mDateView.setOnClickListener(this);
+        mClockDateView = findViewById(R.id.date_clock);
+        mSecurityHeaderView = findViewById(R.id.header_text_container);
+        mClockIconsSeparator = findViewById(R.id.separator);
+        mRightLayout = findViewById(R.id.rightLayout);
+        mDateContainer = findViewById(R.id.date_container);
+        mPrivacyContainer = findViewById(R.id.privacy_container);
+
+        mClockContainer = findViewById(R.id.clock_container);
+        mClockView = findViewById(R.id.clock);
+        mClockView.setOnClickListener(this);
+        mClockView.setOnLongClickListener(this);
+        mDatePrivacySeparator = findViewById(R.id.space);
+        // Tint for the battery icons are handled in setupHost()
+        mBatteryRemainingIcon = findViewById(R.id.batteryRemainingIcon);
 
         updateResources();
     }
@@ -59,10 +147,57 @@ public class QuickStatusBarHeader extends FrameLayout {
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        // Only react to touches inside QuickQSPanel
-        if (event.getY() > mHeaderQsPanel.getTop()) {
-            return super.onTouchEvent(event);
+    public void onRtlPropertiesChanged(int layoutDirection) {
+        super.onRtlPropertiesChanged(layoutDirection);
+        updateResources();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mClockView) {
+            mActivityStarter.postStartActivityDismissingKeyguard(new Intent(
+                    AlarmClock.ACTION_SHOW_ALARMS), 0);
+        } else if (v == mDateView) {
+            final Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+            builder.appendPath("time");
+            builder.appendPath(Long.toString(System.currentTimeMillis()));
+            final Intent todayIntent = new Intent(Intent.ACTION_VIEW, builder.build());
+            mActivityStarter.postStartActivityDismissingKeyguard(todayIntent, 0);
+        }
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        if (v == mClockView || v == mDateView) {
+            final Intent nIntent = new Intent(Intent.ACTION_MAIN);
+            nIntent.setClassName("com.android.settings",
+                    "com.android.settings.Settings$DateTimeSettingsActivity");
+            mActivityStarter.startActivity(nIntent, true /* dismissShade */);
+            mVibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+            return true;
+        }
+        return false;
+    }
+
+    private void setDatePrivacyContainersWidth(boolean landscape) {
+        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) mDateContainer.getLayoutParams();
+        lp.width = landscape ? WRAP_CONTENT : 0;
+        lp.weight = landscape ? 0f : 1f;
+        mDateContainer.setLayoutParams(lp);
+
+        lp = (LinearLayout.LayoutParams) mPrivacyContainer.getLayoutParams();
+        lp.width = landscape ? WRAP_CONTENT : 0;
+        lp.weight = landscape ? 0f : 1f;
+        mPrivacyContainer.setLayoutParams(lp);
+    }
+
+    private void setSecurityHeaderContainerVisibility(boolean landscape) {
+        mSecurityHeaderView.setVisibility(landscape ? VISIBLE : GONE);
+    }
+
+    private void updateBatteryMode() {
+        if (mConfigShowBatteryEstimate) {
+            mBatteryRemainingIcon.setPercentShowMode(BatteryMeterView.MODE_ESTIMATE);
         } else {
             return false;
         }
