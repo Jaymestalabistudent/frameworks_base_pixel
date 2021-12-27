0fe4d2cdc3a7d33b2afc43fe3f58ac400dee84fc
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-27 17:05:57 +0700

    OrchidOs system ui : Enhanced Transparent QS customization [1/2]
    Google's dual-tone QS design where the notification panel has a
    semantically higher elevation adds depth to the notification+QS shade,
    and we don't necessarily have to give it up just because our QS has
    light and dark themes.
    
    To preserve the dual-tone effect, use a darker background color for the
    QS section:
    
    Light:
        Notifications: neutral1 20 (surface_light)
        Notification panel: neutral1 50 (light BG)
        QS background: neutral1 100 (darker light BG / surface_header_light)
        Inactive QS tiles: neutral1 20 (surface_light)
    
    Dark:
        Notifications: neutral1 800 (surface_dark)
        Notification panel: neutral1 900 (dark BG)
        QS background: neutral1 950 (surface_header_dark_sysui modulated to L* 5)
        Inactive QS tiles: neutral1 800 (surface_dark)
    
    The dark QS background could be neutral1 0 (black) like it was before,
    but I don't think it looks as good because it can't be tinted based on
    the active wallpaper and thus stands out from other colors.
    
    Unfortunately, Google's current CAM16-based modulation causes hue shifts
    in extreme light and dark shades (e.g. L* = 98 / 5), but we'll fix this
    by generating and overlaying modulated surface colors in our
    ThemeOverlayController implementation.
    
    Demo screenshots: https://twitter.com/kdrag0n/status/1445922541218922496
    
    Change-Id: Icdc4957ecb4e0201377351f1a3e1c6928d6b3955
    Signed-off-by: Fiqri Ardyansyah <fiqri19102k2@gmail.com>
    
    OrchidOs system ui : Transparent QS customization [1/2]
    
    Inspired by dotOS
    
    @neobuddy89: Clean up unused code and convert to 1-100 scale.
    Change-Id: I166326cfe6a19dc9b3d5f5c491d7e87e9c6a1913
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    ScrimController: User Tuner API to apply custom alpha
    
    Change-Id: I0a3486167be3e8d5942af044abfa86ff8572dcc4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    SystemUI: Fix multiple QS background scrim issues
    
    * Apply custom scrim alpha correctly.
    * Fixes: Flickering when using custom scrim alpha and
      expanding notifications from secured lock screen
    * Fixes: https://github.com/crdroidandroid/issue_tracker/issues/110
    
    Change-Id: I147a250b26528333808ba257d2b8ee943d5c9644
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Fix scrim issues regarding transparency
    
    * Fix bouncer background not animating
    * Fix screen flickering when opening scrimmed bouncer
    * Fix SHADE_LOCKED ignoring custom alpha
    * Fix AOSP issue: Fix scrimmed bouncer background animating to intermediate state
    
    Change-Id: I8d4ce2a2512b6859e55f645288eb59a4d4f944b7
    
    SystemUI: Apply QS transparency to footer actions background
    
    * Making footer completely transparent looks bad with floating buttons.
      and keeping it completely opaque looks odd when rest of QS has transparency applied
    
    Change-Id: I06b39f429f0a905a8e2adf0e045bd1c07afb2bb3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    fixup! ScrimController: User Tuner API to apply custom alpha
    
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>
    
    OrchidOs Gamespace import : Add support for GameSpace
    
    This is an addon for GameSpace for broadcasting about game start/stop.
    Additionally, it also has special option for suppressing fullscreen
    intent like incoming call.
    
    This also squash commits:
    - SystemUI: Improve GameSpace lifecycle and broadcast handling
    - GameSpaceManager: Handle various case against "locking screen" scenario
    - GameSpace: Fix issue with multiple users [neobuddy89]
    - SystemUI: Update gamespace for A13 [Genkzsz11]
    - GameSpaceManager: restrict broadcast to MANAGE_GAME_MODE holders only
    - GameSpaceManager: Update TaskStack listener usage
    
    Change-Id: I414802c6307d5849ac9be9b7e6f7d8ae95b54fa4
    
    Fonts: Add regular / light font config
    
    - Required for GameSpace integration and other features
    
    Change-Id: Ibaf1ca73411865980014c573ad49cad5218930a6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5802,6 +5802,16 @@ public final class Settings {
         public static final String RETICKER_COLORED = "reticker_colored";
 
         /**
+         * @hide
+         */
+        public static final String QS_TRANSPARENCY = "qs_transparency";
+        
+        /**
+         * @hide
+         */
+        public static final String QS_TRANSPARENCY = "qs_transparency";
+        
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
