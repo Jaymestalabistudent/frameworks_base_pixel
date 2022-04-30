e1f0c9275026e30f583a6f022aab08e75f79d9c4
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    OrchidOs UI : Transparent QS customization
    Inspired by dotOS
    
    neobuddy89: Clean up unused code and convert to 1-100 scale.
    Change-Id: If1359191bbf1628143bde028bc947e3765f513fe
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    ScrimController: User Tuner API to apply custom alpha
    
    mnri: use dependency
    Change-Id: I0a3486167be3e8d5942af044abfa86ff8572dcc4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    SystemUI: Fix multiple QS background scrim issues
    
    * Apply custom scrim alpha correctly.
    * Fixes: Flickering when using custom scrim alpha and
      expanding notifications from secured lock screen
    * Fixes: https://github.com/crdroidandroid/issue_tracker/issues/110
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Fix scrim issues regarding transparency
    
    * Fix bouncer background not animating
    * Fix screen flickering when opening scrimmed bouncer
    * Fix SHADE_LOCKED ignoring custom alpha
    * Fix AOSP issue: Fix scrimmed bouncer background animating to intermediate state
    
    Change-Id: I8d4ce2a2512b6859e55f645288eb59a4d4f944b7
    
    SystemUI: Apply QS transparency to footer actions background
    
    * Making footer completely transparent looks bad with floating buttons.
      and keeping it completely opaque looks odd when rest of QS has transparency applied
    
    mnri: use Dependency
    
    Change-Id: I06b39f429f0a905a8e2adf0e045bd1c07afb2bb3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -28,6 +28,7 @@ import android.content.res.Configuration;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Trace;
+import android.provider.Settings;
 import android.util.IndentingPrintWriter;
 import android.util.Log;
 import android.view.ContextThemeWrapper;
@@ -45,6 +46,7 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LifecycleRegistry;
 
 import com.android.keyguard.BouncerPanelExpansionCalculator;
+import com.android.systemui.Dependency;
 import com.android.systemui.Dumpable;
 import com.android.systemui.R;
 import com.android.systemui.animation.Interpolators;
@@ -83,12 +85,15 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Callbacks,
-        StatusBarStateController.StateListener, Dumpable {
+        StatusBarStateController.StateListener, Dumpable, TunerService.Tunable {
     private static final String TAG = "QS";
     private static final boolean DEBUG = false;
     private static final String EXTRA_EXPANDED = "expanded";
     private static final String EXTRA_LISTENING = "listening";
     private static final String EXTRA_VISIBLE = "visible";
+
+    private static final String QS_TRANSPARENCY =
+            "system:" + Settings.System.QS_TRANSPARENCY;
 
     private final Rect mQsBounds = new Rect();
     private final SysuiStatusBarStateController mStatusBarStateController;
@@ -168,6 +173,8 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
     private boolean mQsVisible;
 
     private boolean mIsSmallScreen;
+
+    private float mCustomAlpha = 1f;
 
     @Inject
     public QSFragment(RemoteInputQuickSettingsDisabler remoteInputQsDisabler,
@@ -283,6 +290,8 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
                     mQSPanelController.getMediaHost().getHostView().setAlpha(1.0f);
                     mQSAnimator.requestAnimatorUpdate();
                 });
+
+        Dependency.get(TunerService.class).addTunable(this, QS_TRANSPARENCY);
     }
 
     private void bindFooterActionsView(View root) {
@@ -412,6 +421,18 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
     @Override
     public void setCollapsedMediaVisibilityChangedListener(Consumer<Boolean> listener) {
         mQuickQSPanelController.setMediaVisibilityChangedListener(listener);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        switch (key) {
+            case QS_TRANSPARENCY:
+                mCustomAlpha =
+                        TunerService.parseInteger(newValue, 100) / 100f;
+                break;
+            default:
+                break;
+         }
     }
 
     private void setEditLocation(View view) {
@@ -668,7 +689,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
         float footerActionsExpansion =
                 onKeyguardAndExpanded ? 1 : mInSplitShade ? alphaProgress : expansion;
         mQSFooterActionsViewModel.onQuickSettingsExpansionChanged(footerActionsExpansion,
-                mInSplitShade);
+                mInSplitShade, mCustomAlpha);
         mQSPanelController.setRevealExpansion(expansion);
         mQSPanelController.getTileLayout().setExpansion(expansion, proposedTranslation);
         mQuickQSPanelController.getTileLayout().setExpansion(expansion, proposedTranslation);
