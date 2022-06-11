072ae2159a7cccb571ab131c0f969b55f1aab35a
LibXZR <i@xzr.moe>
2022-06-11 16:13:53 +0800

    base: Add support for strict standby policy
    When dismissing from recents, this will force stop apps with restricted
    battery usage and put them into RESTRICTED standby bucket. Also all
    restrictions will be imposed even when device is charging.
    
    This can be useful to kill dirty vigorous Chinese apps.
    
    Change-Id: I8f075ef48c5775971e3e0b8b1ea843d70a2e44c4
    Signed-off-by: LibXZR <i@xzr.moe>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -18759,4 +18759,12 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
     }
 
+  
+
+    boolean shouldSkipBootCompletedBroadcastForPackage(ApplicationInfo info) {
+        return mActivityTaskManager.mAppStandbyInternal.isStrictStandbyPolicyEnabled() &&
+                getAppOpsManager().checkOpNoThrow(
+                        AppOpsManager.OP_RUN_ANY_IN_BACKGROUND,
+                        info.uid, info.packageName) != AppOpsManager.MODE_ALLOWED;
+    }
 }
