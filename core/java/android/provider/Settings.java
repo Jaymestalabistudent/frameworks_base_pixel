03f43472d99a4f841a0ed9dc3c104c2af2fa2295
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-11-29 22:03:03 +0530

    OrchidOs vibr : Add optional haptic feedback to brightness slider [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6061,6 +6061,12 @@ public final class Settings {
 
        
         /**
+         * Haptic feedback on brightness slider
+         * @hide
+         */ 
+        public static final String BRIGHTNESS_SLIDER_HAPTIC_FEEDBACK = "brightness_slider_haptic_feedback";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
