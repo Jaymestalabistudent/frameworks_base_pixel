b85eca09b8f57973be56a46a385f6c2c9d70cc6e
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-17 19:46:08 +0530

    SystemUI: QS Header Image [1/2]
    * reverse engineered from ancient os with [1] as reference for broken codes from jadx
    * Optimized qs banner resources for systemui file size
    * all credits goes to ancientOS
    
    [1]: https://github.com/Hikari-no-Tenshi/crdroid_android_frameworks_base/commit/354d981d3f79783e284c8aced755e90496324982
    
    mnri: rewrite and refactored
    
    Co-authored-by: Idc <59904122+IDontCare-05@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Squashed:
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Wed, 26 Apr 2023 03:17:27 +0530
        Subject: [PATCH] SystemUI: Make QS Header Image less intrusive with codebase
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Idc <59904122+IDontCare-05@users.noreply.github.com>
        Date: Wed, 10 May 2023 05:41:46 +0800
        Subject: [PATCH] SystemUI: Import more QS Header resources
    
        * decompiled resources from ancientOS, all credits goes to respective owners
    
        @neobuddy89: Drop anime images with artist names, drop repeated scenaries and patterns.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6296,6 +6296,14 @@ public final class Settings {
          * @hide
          */
         public static final String QQS_LAYOUT = "qqs_layout";
+
+     
+        /**
+         * QS Header
+         * @hide
+         */
+        public static final String QS_HEADER_IMAGE = "qs_header_image";
+
         /**
          * These are all public system settings
          *
