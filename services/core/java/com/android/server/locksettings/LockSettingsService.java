fb59a7c39ea0cab0b55d7fefb475f579efe374cb
theprojects <talabijay@gmail.com>
2023-10-22 12:49:31 +0100

    Revert "Forward port CM Screen Security settings (1/2)"
    This reverts commit 81877cd8670033b20de086b5fc782b813fe2d64d.

--- services/core/java/com/android/server/locksettings/LockSettingsService.java
+++ services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -1605,10 +1605,6 @@ public class LockSettingsService extends ILockSettings.Stub {
                 && getSeparateProfileChallengeEnabledInternal(userId);
     }
 
-    public byte getLockPatternSize(int userId) {
-        return mStorage.getLockPatternSize(userId);
-    }
-
     /**
      * Send credentials for user {@code userId} to {@link RecoverableKeyStoreManager} during an
      * unlock operation.
@@ -2699,10 +2695,7 @@ public class LockSettingsService extends ILockSettings.Stub {
             Secure.LOCK_PATTERN_ENABLED,
             Secure.LOCK_BIOMETRIC_WEAK_FLAGS,
             Secure.LOCK_PATTERN_VISIBLE,
-            Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED,
-            Secure.LOCK_PATTERN_SIZE,
-            Secure.LOCK_DOTS_VISIBLE,
-            Secure.LOCK_SHOW_ERROR_PATH
+            Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED
     };
 
     // Reading these settings needs the contacts permission
