fc61871829b0473e4b471530fbd9ff977ce852af
theprojects <talabijay@gmail.com>
2023-11-03 11:24:43 +0000

    Revert "OrchidOs QS:  QS Header-image[1/2]"
    This reverts commit b9329f7a3b97b10b304f43bc8b2053b4e5976465.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5820,11 +5820,6 @@ public final class Settings {
         public static final String SMART_5G = "smart_5g";
 
         /**
-         * QS Header
-         * @hide
-         */
-        public static final String QS_HEADER_IMAGE = "qs_header_image";
-        /**
          * These are all public system settings
          *
          * @hide
