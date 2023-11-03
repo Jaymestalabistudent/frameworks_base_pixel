89fe66934e15637b16722cadc462b6d0802c58bf
theprojects <talabijay@gmail.com>
2023-11-03 21:55:24 +0000

    Revert "OrhidOs UI : Add CPU info overlay"
    This reverts commit fc6d85958764328cbc5ea9c401f138ca2f7222c8.

--- packages/SystemUI/src/com/android/systemui/custom/CustomQSModule.kt
+++ packages/SystemUI/src/com/android/systemui/custom/CustomQSModule.kt
@@ -11,7 +11,6 @@ import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.VpnTile
-import com.android.systemui.qs.tiles.CPUInfoTile
 
 import dagger.Binds
 import dagger.Module
@@ -37,12 +36,6 @@ interface CustomQSModule {
     @IntoMap
     @StringKey(WifiTile.TILE_SPEC)
     fun bindWifiTile(wifiTile: WifiTile): QSTileImpl<*>
-   
-    /** Inject CPUInfoTile into tileMap in QSModule */
-    @Binds
-    @IntoMap
-    @StringKey(CPUInfoTile.TILE_SPEC)
-    fun bindCPUInfoTile(cpuInfoTile: CPUInfoTile): QSTileImpl<*>
 
     /** Inject CaffeineTile into tileMap in QSModule */
     @Binds
