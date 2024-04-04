a592671d3858b10a6f3d583c78c994ff5442bbf2
theprojects <talabijaymes@gmail.com>
2024-04-04 20:43:17 +0100

    Revert "OrchidOs Privacy : Add support for hiding privacy indicator"
    This reverts commit 3d60ba9c88b1b41f5f7e8678a5ea2ef40cef3ec6.

--- packages/SystemUI/src/com/android/systemui/statusbar/events/SystemEventCoordinator.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/events/SystemEventCoordinator.kt
@@ -20,7 +20,6 @@ import android.annotation.IntRange
 import android.content.Context
 import android.provider.DeviceConfig
 import android.provider.DeviceConfig.NAMESPACE_PRIVACY
-import android.provider.Settings
 import com.android.systemui.R
 import com.android.systemui.dagger.SysUISingleton
 import com.android.systemui.flags.FeatureFlags
@@ -29,7 +28,6 @@ import com.android.systemui.privacy.PrivacyChipBuilder
 import com.android.systemui.privacy.PrivacyItem
 import com.android.systemui.privacy.PrivacyItemController
 import com.android.systemui.statusbar.policy.BatteryController
-import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.time.SystemClock
 import javax.inject.Inject
 
@@ -43,23 +41,18 @@ class SystemEventCoordinator @Inject constructor(
     private val batteryController: BatteryController,
     private val privacyController: PrivacyItemController,
     private val context: Context,
-    private val tunerService: TunerService
+    private val featureFlags: FeatureFlags
 ) {
     private lateinit var scheduler: SystemStatusAnimationScheduler
-    private var hidePrivacyIndicator: Boolean = false
 
     fun startObserving() {
         batteryController.addCallback(batteryStateListener)
-        */
-        tunerService.addTunable(hidePrivacyIndicatorTunable,
-                Settings.Secure.HIDE_PRIVACY_INDICATOR)
         privacyController.addCallback(privacyStateListener)
     }
 
     fun stopObserving() {
         batteryController.removeCallback(batteryStateListener)
         privacyController.removeCallback(privacyStateListener)
-        tunerService.removeTunable(hidePrivacyIndicatorTunable)
     }
 
     fun attachScheduler(s: SystemStatusAnimationScheduler) {
@@ -124,11 +117,10 @@ class SystemEventCoordinator @Inject constructor(
             }
 
             currentPrivacyItems = privacyItems
-            if (!hidePrivacyIndicator)
-                notifyListeners()
+            notifyListeners()
         }
 
-        fun notifyListeners() {
+        private fun notifyListeners() {
             if (currentPrivacyItems.isEmpty()) {
                 notifyPrivacyItemsEmpty()
             } else {
@@ -147,19 +139,6 @@ class SystemEventCoordinator @Inject constructor(
 
         private fun isChipAnimationEnabled(): Boolean {
             return DeviceConfig.getBoolean(NAMESPACE_PRIVACY, CHIP_ANIMATION_ENABLED, true)
-        }
-    }
-
-    private val hidePrivacyIndicatorTunable = object: TunerService.Tunable {
-        override fun onTuningChanged(key: String, newValue: String?) {
-            val newHidePrivacyIndicator = TunerService.parseIntegerSwitch(newValue, false)
-            if (hidePrivacyIndicator == newHidePrivacyIndicator)
-                return
-            hidePrivacyIndicator = newHidePrivacyIndicator
-            if (hidePrivacyIndicator)
-                notifyPrivacyItemsEmpty()
-            else
-                privacyStateListener.notifyListeners()
         }
     }
 }
